4a4d06c1508dc329c3fab65b4d3f53e8ba6c5c4e
root <root@ip-172-31-47-202.ap-northeast-3.compute.internal>
2024-11-06 06:50:21 +0000

    aws

---
+++ homepage/src/main/java/instagram/sample.java
@@ -0,0 +1,13 @@
+package instagram;
+
+/**
+ * Hello world!
+ *
+ */
+public class sample
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World1234!" );
+    }
+}
